161f67ed26f94b19df0cf8dea255fc17cd14b602
satyam-software-developer <kr.satyam0506@gmail.com>
2026-01-23 05:10:01 +0530

    all files are committed

---
+++ src/main/java/com/coding/LearningRESTAPIs/dto/AddStudentRequestDto.java
@@ -0,0 +1,23 @@
+package com.coding.LearningRESTAPIs.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class AddStudentRequestDto {
+
+    @NotBlank(message = "Name is required")
+    @Size(min = 3, max = 30, message = "Name should be of length 3 to 30 characters")
+    private String name;
+
+    @Email
+    @NotBlank(message = "Email is Required")
+    private String email;
+}
+
